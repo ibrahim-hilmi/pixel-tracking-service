223275e0a2564fc39f66f70805c84b6da72bc07d
İbrahim Hilmi Akargül <ibrahim.akargul+atlassian.com@gmail.com>
2023-03-22 08:30:35 +0300

    Added pixel param

--- src/main/java/com/apibinder/pixel/controller/PixelController.java
+++ src/main/java/com/apibinder/pixel/controller/PixelController.java
@@ -3,23 +3,21 @@ package com.apibinder.pixel.controller;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/pixel")
 public class PixelController {
 
-    @GetMapping(
-            produces = MediaType.IMAGE_PNG_VALUE
-    )
-    public @ResponseBody byte[] getImage() throws IOException {
+
+    @GetMapping(value = "{pixelId}", produces = MediaType.IMAGE_PNG_VALUE)
+    public @ResponseBody byte[] getImage(@RequestParam(required = false) Map<String, String> requestParams,
+                                         @PathVariable(required = true) String pixelId) throws IOException {
         InputStream in = getClass()
                 .getResourceAsStream("/static/pixel.png");
         return IOUtils.toByteArray(in);
